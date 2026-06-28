98630474aa80594a19ae86991439f24fedd6ded0
evadivas3 <blueoceansparkle@gmail.com>
2026-06-28 00:14:20 -0500

    attention_pool made: returns pooled embedding and attention weights for interpretability later on

---
+++ models/attention_pool.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class condition_attention_pool(nn.Module):
+    def __init__(self,d_model=64,num_cons=9):
+        super().__init__()
+        self.d_model=d_model
+        self.num_cons=num_cons
+        self.attention = nn.Linear(d_model, 1)
+    def forward(self, embed):
+        a=self.attention(embed)
+        b=a.squeeze(-1)
+        c=F.softmax(b,dim=0)
+        weight_sum=torch.einsum('i,ij->j',c,embed)
+        return (weight_sum,c)
