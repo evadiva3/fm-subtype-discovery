23dfc5796c32d7383367e937b5422b5e6fbadc17
evadivas3 <blueoceansparkle@gmail.com>
2026-07-05 21:33:22 -0500

    attention pool add temp

--- models/attention_pool.py
+++ models/attention_pool.py
@@ -7,13 +7,12 @@ class condition_attention_pool(nn.Module):
         super().__init__()
         self.d_model=d_model
         self.num_cons=num_cons
+        self.tau=nn.Parameter(torch.ones(1))
         self.attention = nn.Linear(d_model, 1)
     def forward(self, embed):
         a=self.attention(embed)
         b=a.squeeze(-1)
-        c=F.softmax(b,dim=0)
+        c=F.softmax(b/self.tau, dim=0)
         weight_sum=torch.einsum('i,ij->j',c,embed)
-        return (weight_sum,c)
-#Nikhil: looking at this, attention_pool uses nn.Linear which I am pretty sure is a linear model and has learnable featuree
-# and weights so I could've sworn that using it as is and not training it at some point would mean it when I use
-# it for cluster it would initialize with random weights which I don't think would be good. idk you can ask claude.wh
+        return (weight_sum, c, self.tau)
+#addressed
