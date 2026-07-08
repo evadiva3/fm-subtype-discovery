28c3e087f8e8192c813d946c1ce2916faf86992c
Nikhil Joshi <inferno18c@gmail.com>
2026-07-07 20:33:43 -0500

    working on ablate4

--- preprocessing/compute_fc_matrices.py
+++ preprocessing/compute_fc_matrices.py
@@ -31,16 +31,15 @@ class preprocessBOLD:
         try:
             confound = pd.read_csv(self.pathToConfoundsFile, sep="\t");
             selectedConfounds = ["global_signal", "white_matter", "csf", "trans_x", "trans_x_derivative1", "trans_x_derivative1_power2", "trans_x_power2", "trans_y", "trans_y_derivative1", "trans_y_power2", "trans_y_derivative1_power2", "trans_z", "trans_z_derivative1", "trans_z_derivative1_power2", "trans_z_power2", "rot_x", "rot_x_derivative1", "rot_x_power2", "rot_x_derivative1_power2", "rot_y", "rot_y_derivative1", "rot_y_power2", "rot_y_derivative1_power2", "rot_z", "rot_z_derivative1", "rot_z_power2", "rot_z_derivative1_power2"]; 
-
             cleanedConfounds = confound[selectedConfounds].fillna(0);
             return self.masker.fit_transform(self.pathToBOLDFile, confounds=cleanedConfounds);
         except Exception as error:
             raise RuntimeError("Check Pathname Hardcodes") from error;
-
     def splitConditions(self):
         timeSeries = self.buildTimeSeries();
         tsvData = pd.read_csv(self.eventsListPath, sep="\t");
-        conditionTimeSeries = [[] for _ in range(0, 7)]; conditionFrames = [[] for _ in range(0, 7)];
+        conditionTimeSeries = [[] for _ in range(0, 7)]; 
+        conditionFrames = [[] for _ in range(0, 7)];
         for row in tsvData.itertuples():
             for i in range(len(self.conditions)):
                 if row.trial_type == self.conditions[i]:
@@ -51,11 +50,10 @@ class preprocessBOLD:
             conditionFrames[condition] = pd.DataFrame(np.concatenate(conditionTimeSeries[condition]));
         self.saveTimeSeries = [frame.to_numpy() for frame in conditionFrames];
         return conditionFrames;
-
-    # this method needs to use Ledoit-Wolf and proper Fisher z shifting;
     def buildFCMatrices(self):
         conditionFrames = self.splitConditions();
-        fcMatrices = [[] for _ in range(7)]; lwMeasure = ConnectivityMeasure(kind="correlation", cov_estimator=LedoitWolf());
+        fcMatrices = [[] for _ in range(7)]; 
+        lwMeasure = ConnectivityMeasure(kind="correlation", cov_estimator=LedoitWolf());
         for i in range(len(conditionFrames)):
             array = conditionFrames[i].to_numpy();
             rMatrix = lwMeasure.fit_transform([array])[0];
@@ -64,22 +62,14 @@ class preprocessBOLD:
             zMatrix[np.isinf(zMatrix)] = 0;
             fcMatrices[i] = zMatrix;
         return fcMatrices;
-
-    # this is a new method that converts the matrix objects into PyTorch Geometric graph topologies;
     def buildSparse(self, fcMatrix, topK):
         numNodes = fcMatrix.shape[0];
         meanConn = fcMatrix.mean(axis=1);
         varConn = fcMatrix.var(axis=1);
         degree = (np.abs(fcMatrix) > 0).sum(axis=1).astype(float);
         spectral = np.linalg.norm(fcMatrix, axis=1);
-        clust = np.array([
-            fcMatrix[i, np.argsort(np.abs(fcMatrix[i]))[-topK:]].mean()
-            for i in range(numNodes)
-        ]);
-        x = torch.tensor(
-            np.stack([meanConn, varConn, degree, clust, spectral], axis=1),
-            dtype=torch.float,
-        );
+        clust = np.array([fcMatrix[i, np.argsort(np.abs(fcMatrix[i]))[-topK:]].mean()for i in range(numNodes)]);
+        x = torch.tensor(np.stack([meanConn, varConn, degree, clust, spectral], axis=1),dtype=torch.float,);
         sources, targets, edgeWeights = [], [], [];
         for nodeIdx in range(numNodes):
             row = fcMatrix[nodeIdx];
@@ -91,8 +81,6 @@ class preprocessBOLD:
         edgeIndex = torch.tensor([sources, targets], dtype=torch.long);
         edgeAttr = torch.tensor(edgeWeights, dtype=torch.float).unsqueeze(-1);
         return Data(x=x, edge_index=edgeIndex, edge_attr=edgeAttr);
-
-    # method to iterate and arrange by interpolated edge density;
     def execute(self):
         try:
             topKList = [20, 30, 40, 50];
