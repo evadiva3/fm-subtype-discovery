d7720162ed21f9c76419dc2ca269bcfcefe78958
evadivas3 <blueoceansparkle@gmail.com>
2026-07-11 14:44:45 -0500

    fix pandas3 group dtype crash, clinical csv now generates

--- dataFiltering/data.py
+++ dataFiltering/data.py
@@ -1,4 +1,11 @@
 import pandas as pd;
+import sys
+from pathlib import Path
+
+_ROOT=Path(__file__).resolve().parent.parent
+if str(_ROOT) not in sys.path:
+    sys.path.insert(0, str(_ROOT))
+
 from config import config;
 pathToXML = config.clinicalXlsx;
 pathToCSV = config.clinicalCsv;
@@ -25,6 +32,10 @@ def getStatData():
     filteredList = data[['rid','gp','1_age','1_vas_pain_iv','6_hamd_total','6_hamd_cat','7_hama_total','11_ts','11_ id', '11_df','11_eot', '11_cat','10_R','10_S']].copy();
     filteredList = filteredList.rename(columns=columns);
     filteredList['subject_id'] =  "sub-"+filteredList['subject_id'].astype(str).str.zfill(3);
+    filteredList['group']=filteredList['group'].astype(object)
     filteredList.loc[filteredList['group']== 0,'group'] = "FM";
     filteredList.loc[filteredList['group']==1,'group'] = "HC";
     filteredList.to_csv(pathToCSV, index=False);
+
+if __name__ == "__main__":
+    getStatData()
