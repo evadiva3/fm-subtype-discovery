929026a8bfa0d0a97729e8d0960e1182fc2cfc74
Nikhil Joshi <inferno18c@gmail.com>
2026-07-14 11:41:26 -0500

    Use valLoss as optuna metric instead of silhouette score

--- src/train.py
+++ src/train.py
@@ -203,7 +203,7 @@ def joint_train(model,attention_pool,loss_fn,dataloader,val_dataloader,augmentor
     if realData is not None:
         direct = tune.get_context().get_trial_dir();
         bestScore = intermedCluster(realData, model, attention_pool, direct);
-        tune.report({"valLoss": avg_val_loss, "silhouetteScore": bestScore});
+        tune.report({"valLoss": avg_val_loss});
     return model, attention_pool, train_losses, val_losses
 def intermedCluster(data, encodeOut, attentionOut, direct):
     from clustering import cluster;
